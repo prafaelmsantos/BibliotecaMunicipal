f327b0d7eaeeecd2ae874636ff76df2b68a85b3a
Rafael Santos <prafaelmsantos@gmail.com>
2023-03-01 00:57:36 +0000

    first commit

---
+++ BibliotecaMunicipal/Models/Contexts/BibliotecaMunicipalContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BibliotecaMunicipal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaMunicipal.Contexts
+{
+    public class BibliotecaMunicipalContext: DbContext
+    {
+        public BibliotecaMunicipalContext(DbContextOptions<BibliotecaMunicipalContext> options) : base(options) { }
+
+        public DbSet<Livro> Livros { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+        }
+    }
+}
